44e941bc2014a232012626405a2ed3d8a485a126
Peter DeVita <mewtwo2643@yahoo.com>
2023-07-16 17:55:27 -0400

    Disable top level __all__ for now to fix docs

--- atsume/__init__.py
+++ atsume/__init__.py
@@ -13,12 +13,12 @@ from .component import (
     as_time_schedule,
 )
 
-__all__ = [
-    "with_listener",
-    "Component",
-    "ComponentConfig",
-    "Context",
-    "on_open",
-    "on_close",
-    "as_time_schedule",
-]
+# __all__ = [
+#     "with_listener",
+#     "Component",
+#     "ComponentConfig",
+#     "Context",
+#     "on_open",
+#     "on_close",
+#     "as_time_schedule",
+# ]
