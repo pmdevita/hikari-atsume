d935a59bc2dd8a84e8c4adf8b76ec7c06716b949
Peter DeVita <mewtwo2643@yahoo.com>
2026-07-11 15:10:12 -0400

    Add migration for Handles

---
+++ atsume/contrib/handles/migrations/handles_2026_04_01t00_50_01_170245.py
@@ -0,0 +1,240 @@
+from piccolo.apps.migrations.auto.migration_manager import MigrationManager
+from piccolo.columns.column_types import BigInt, Boolean, Serial, Varchar
+from piccolo.columns.indexes import IndexMethod
+
+ID = "2026-04-01T00:50:01:170245"
+VERSION = "1.30.0"
+DESCRIPTION = ""
+
+
+async def forwards():
+    manager = MigrationManager(
+        migration_id=ID, app_name="handles", description=DESCRIPTION
+    )
+
+    manager.add_table(
+        class_name="ComponentDM",
+        tablename="component_dm",
+        schema=None,
+        columns=None,
+    )
+
+    manager.add_table(
+        class_name="ComponentChannelHandle",
+        tablename="component_channel_handle",
+        schema=None,
+        columns=None,
+    )
+
+    manager.add_column(
+        table_class_name="ComponentDM",
+        tablename="component_dm",
+        column_name="id",
+        db_column_name="id",
+        column_class_name="Serial",
+        column_class=Serial,
+        params={
+            "null": False,
+            "primary_key": True,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="ComponentDM",
+        tablename="component_dm",
+        column_name="component",
+        db_column_name="component",
+        column_class_name="Varchar",
+        column_class=Varchar,
+        params={
+            "length": 255,
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="ComponentDM",
+        tablename="component_dm",
+        column_name="mode",
+        db_column_name="mode",
+        column_class_name="Boolean",
+        column_class=Boolean,
+        params={
+            "default": True,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="ComponentChannelHandle",
+        tablename="component_channel_handle",
+        column_name="id",
+        db_column_name="id",
+        column_class_name="Serial",
+        column_class=Serial,
+        params={
+            "null": False,
+            "primary_key": True,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="ComponentChannelHandle",
+        tablename="component_channel_handle",
+        column_name="component",
+        db_column_name="component",
+        column_class_name="Varchar",
+        column_class=Varchar,
+        params={
+            "length": 255,
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="ComponentChannelHandle",
+        tablename="component_channel_handle",
+        column_name="handle_name",
+        db_column_name="handle_name",
+        column_class_name="Varchar",
+        column_class=Varchar,
+        params={
+            "length": 255,
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="ComponentChannelHandle",
+        tablename="component_channel_handle",
+        column_name="guild_id",
+        db_column_name="guild_id",
+        column_class_name="BigInt",
+        column_class=BigInt,
+        params={
+            "default": 0,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="ComponentChannelHandle",
+        tablename="component_channel_handle",
+        column_name="channel_id",
+        db_column_name="channel_id",
+        column_class_name="BigInt",
+        column_class=BigInt,
+        params={
+            "default": 0,
+            "null": True,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="ComponentChannelHandle",
+        tablename="component_channel_handle",
+        column_name="all",
+        db_column_name="all",
+        column_class_name="Boolean",
+        column_class=Boolean,
+        params={
+            "default": False,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="ComponentChannelHandle",
+        tablename="component_channel_handle",
+        column_name="mode",
+        db_column_name="mode",
+        column_class_name="Boolean",
+        column_class=Boolean,
+        params={
+            "default": True,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    return manager
